01f483f5ffd040ba378a7119185227e184d685f6
Enhex <enhex0@gmail.com>
2022-11-06 22:32:09 +0200

    use downloaded ClangBuildAnalyzer

--- scripts/measure/boost.sh
+++ scripts/measure/boost.sh
@@ -7,9 +7,10 @@ cd ./boost_1_80_0
 ./bootstrap.sh --with-toolset=clang
 ./b2 clean
 
-ClangBuildAnalyzer --start ./
+../ClangBuildAnalyzer --start ./
 
 ./b2 -j $(nproc) toolset=clang cxxflags="-ftime-trace" cflags="-ftime-trace"
 
-ClangBuildAnalyzer --stop ./ ../boost-capture.bin
-ClangBuildAnalyzer --analyze ../boost-capture.bin > ../boost.txt
+../ClangBuildAnalyzer --stop ./ ../boost-capture.bin
+cd ..
+./ClangBuildAnalyzer --analyze ./boost-capture.bin > ./boost.txt
