597d1cbae1aecfa21e3e3f90891a4d6d1f1ce073
Enhex <enhex0@gmail.com>
2022-11-06 22:31:28 +0200

    avoid redownloading if the file already exists

--- scripts/setup/boost.sh
+++ scripts/setup/boost.sh
@@ -1,3 +1,3 @@
 # download and extract
 cd ../../../build/
-wget -q --show-progress https://boostorg.jfrog.io/artifactory/main/release/1.80.0/source/boost_1_80_0.tar.gz
+wget -q --show-progress -nc https://boostorg.jfrog.io/artifactory/main/release/1.80.0/source/boost_1_80_0.tar.gz
--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 mkdir -p ../build
 
 # get Clang Build Analyzer
-wget -q --show-progress -O ../build/ClangBuildAnalyzer https://github.com/aras-p/ClangBuildAnalyzer/releases/download/v1.4.0/ClangBuildAnalyzer-linux
+wget -q --show-progress -nc -O ../build/ClangBuildAnalyzer https://github.com/aras-p/ClangBuildAnalyzer/releases/download/v1.4.0/ClangBuildAnalyzer-linux
 chmod +x ../build/ClangBuildAnalyzer
 
 # setup projects
